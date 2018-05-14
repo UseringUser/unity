3d5729ad2f4c6a0d5dc6cbcd8e649b50ceea0e46
UseringUser <34933558+UseringUser@users.noreply.github.com>
2018-05-14 16:30:13 +0900

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+	Rigidbody rb;
+	float z;
+	float x;
+	Transform tr;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent <Rigidbody> ();
+		tr = GetComponent <Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		z = Input.GetAxis ("Vertical");
+		x = Input.GetAxis ("Horizontal");
+		rb.MovePosition (tr.position + tr.forward * z);
+		rb.AddForce (tr.forward * z *50f);
+		Vector3 rotation = new Vector3 (0,x,0);
+		tr.Rotate (rotation*2);
+	}
+	void OnCollisionEnter(Collision arg){
+		if(arg.gameObject.tag == "Bounty"){
+			Destroy (arg.gameObject);
+		}
+	}
+}
